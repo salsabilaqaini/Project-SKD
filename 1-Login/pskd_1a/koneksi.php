1a8442ace615944f9d08d002e8cde0de41c125a2
Salsabila Qurrotul 'Aini <96609071+tampungan@users.noreply.github.com>
2021-12-25 01:18:39 +0700

    Delete 1-Login directory

--- 1-Login/pskd_1a/koneksi.php
+++
@@ -1,4 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'root', '', 'pskd01') or die("<h3>MySQL connection is failed.</h3><br>"); 
-
-?>
--- 1-Login/pskd_1b/koneksi.php
+++
@@ -1,5 +0,0 @@
-<?php
-//Koneksi PHP
-$conn = mysqli_connect('localhost', 'root', '', 'pskd01') or die("<h3>MySQL connection is failed.</h3><br>"); 
-
-?>
